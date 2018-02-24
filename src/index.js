922196aa4ef630837db7dcaaed7ec2dbc24b20d3
Andy Tuttle <andytuttle12@gmail.com>
2018-02-24 10:45:15 -0500

    Updating test components.

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    val: PropTypes.number
   }
 
   render() {
     const {
-      text
+      text,
+      val
     } = this.props
 
     return (
       <div className={styles.test}>
         Example Component: {text}
+        The rating for this lib: {val}
       </div>
     )
   }
